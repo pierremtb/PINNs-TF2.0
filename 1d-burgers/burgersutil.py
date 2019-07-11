19c80cb86e2f31e4599ac769afbd180f6475f518
Pierre Jacquier <pierrejacquier39@gmail.com>
2019-07-11 10:19:57 -0400

    Polished the code

--- 1d-burgers/burgersutil.py
+++ 1d-burgers/burgersutil.py
@@ -4,6 +4,23 @@ import tensorflow as tf
 import time
 from datetime import datetime
 from pyDOE import lhs
+import os
+import sys
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+from scipy.interpolate import griddata
+import matplotlib.gridspec as gridspec
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+# "." for Colab, and ".." for GitHub
+# repoPath = os.path.join(".", "PINNs")
+repoPath = os.path.join("..", "PINNs")
+utilsPath = os.path.join(repoPath, "Utilities")
+dataPath = os.path.join(repoPath, "main", "Data")
+appDataPath = os.path.join(repoPath, "appendix", "Data")
+
+sys.path.insert(0, utilsPath)
+from plotting import newfig, savefig
 
 def prep_data(path, N_u, N_f=None, noise=0.0):
     # Reading external data [t is 100x1, usol is 256x100 (solution), x is 256x1]
@@ -90,3 +107,77 @@ class Logger(object):
   def log_train_end(self, epoch, custom=""):
     print("==================")
     print(f"Training finished (epoch {epoch}): duration = {self.__get_elapsed()}  error = {self.__get_error_u():.4e}  " + custom)
+
+def plot_results(X_star, u_pred, X_u_train, u_train, Exact_u, X, T, x, t, file=None):
+
+  # Interpolating the results on the whole (x,t) domain.
+  # griddata(points, values, points at which to interpolate, method)
+  U_pred = griddata(X_star, u_pred, (X, T), method='cubic')
+
+  # Creating the figures
+  fig, ax = newfig(1.0, 1.1)
+  ax.axis('off')
+
+  ####### Row 0: u(t,x) ##################    
+  gs0 = gridspec.GridSpec(1, 2)
+  gs0.update(top=1-0.06, bottom=1-1/3, left=0.15, right=0.85, wspace=0)
+  ax = plt.subplot(gs0[:, :])
+
+  h = ax.imshow(U_pred.T, interpolation='nearest', cmap='rainbow', 
+                extent=[t.min(), t.max(), x.min(), x.max()], 
+                origin='lower', aspect='auto')
+  divider = make_axes_locatable(ax)
+  cax = divider.append_axes("right", size="5%", pad=0.05)
+  fig.colorbar(h, cax=cax)
+
+  ax.plot(X_u_train[:,1], X_u_train[:,0], 'kx', label = 'Data (%d points)' % (u_train.shape[0]), markersize = 4, clip_on = False)
+
+  line = np.linspace(x.min(), x.max(), 2)[:,None]
+  ax.plot(t[25]*np.ones((2,1)), line, 'w-', linewidth = 1)
+  ax.plot(t[50]*np.ones((2,1)), line, 'w-', linewidth = 1)
+  ax.plot(t[75]*np.ones((2,1)), line, 'w-', linewidth = 1)    
+
+  ax.set_xlabel('$t$')
+  ax.set_ylabel('$x$')
+  ax.legend(frameon=False, loc = 'best')
+  ax.set_title('$u(t,x)$', fontsize = 10)
+
+  ####### Row 1: u(t,x) slices ##################    
+  gs1 = gridspec.GridSpec(1, 3)
+  gs1.update(top=1-1/3, bottom=0, left=0.1, right=0.9, wspace=0.5)
+
+  ax = plt.subplot(gs1[0, 0])
+  ax.plot(x,Exact_u[25,:], 'b-', linewidth = 2, label = 'Exact')       
+  ax.plot(x,U_pred[25,:], 'r--', linewidth = 2, label = 'Prediction')
+  ax.set_xlabel('$x$')
+  ax.set_ylabel('$u(t,x)$')    
+  ax.set_title('$t = 0.25$', fontsize = 10)
+  ax.axis('square')
+  ax.set_xlim([-1.1,1.1])
+  ax.set_ylim([-1.1,1.1])
+
+  ax = plt.subplot(gs1[0, 1])
+  ax.plot(x,Exact_u[50,:], 'b-', linewidth = 2, label = 'Exact')       
+  ax.plot(x,U_pred[50,:], 'r--', linewidth = 2, label = 'Prediction')
+  ax.set_xlabel('$x$')
+  ax.set_ylabel('$u(t,x)$')
+  ax.axis('square')
+  ax.set_xlim([-1.1,1.1])
+  ax.set_ylim([-1.1,1.1])
+  ax.set_title('$t = 0.50$', fontsize = 10)
+  ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.35), ncol=5, frameon=False)
+
+  ax = plt.subplot(gs1[0, 2])
+  ax.plot(x,Exact_u[75,:], 'b-', linewidth = 2, label = 'Exact')       
+  ax.plot(x,U_pred[75,:], 'r--', linewidth = 2, label = 'Prediction')
+  ax.set_xlabel('$x$')
+  ax.set_ylabel('$u(t,x)$')
+  ax.axis('square')
+  ax.set_xlim([-1.1,1.1])
+  ax.set_ylim([-1.1,1.1])    
+  ax.set_title('$t = 0.75$', fontsize = 10)
+
+  plt.show()
+
+  if file != None:
+    savefig(file)
--- 1d-burgers/ide_cont_burgers.py
+++ 1d-burgers/ide_cont_burgers.py
@@ -2,28 +2,17 @@
 
 import sys
 import os
-import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
 import tensorflow as tf
 import numpy as np
-from scipy.interpolate import griddata
-from pyDOE import lhs
-import matplotlib.gridspec as gridspec
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # Manually making sure the numpy random seeds are "the same" on all devices, for reproducibility in random processes
 np.random.seed(1234)
 # Same for tensorflow
 tf.random.set_seed(1234)
 
-repoPath = os.path.join("..", "PINNs")
-utilsPath = os.path.join(repoPath, "Utilities")
-dataPath = os.path.join(repoPath, "main", "Data")
-appDataPath = os.path.join(repoPath, "appendix", "Data")
+#%% LOCAL IMPORTS
 
-sys.path.insert(0, utilsPath)
-from plotting import newfig, savefig
-from burgersutil import prep_data, Logger
+from burgersutil import prep_data, Logger, plot_results, appDataPath
 
 #%% HYPER PARAMETERS
 
@@ -45,9 +34,11 @@ class PhysicsInformedNN(object):
     for width in layers[1:]:
         self.u_model.add(tf.keras.layers.Dense(width, activation=tf.nn.tanh))
 
+    self.dtype = tf.float32
+
     # Defining the two additional trainable variables for identification
-    self.lambda_1 = tf.Variable([0.0], dtype=tf.float32)
-    self.lambda_2 = tf.Variable([-6.0], dtype=tf.float32)
+    self.lambda_1 = tf.Variable([0.0], dtype=self.dtype)
+    self.lambda_2 = tf.Variable([-6.0], dtype=self.dtype)
     
     self.optimizer = optimizer
     self.logger = logger
@@ -109,11 +100,11 @@ class PhysicsInformedNN(object):
     self.logger.log_train_start(self.u_model)
 
     # Creating the tensors
-    self.X_u = tf.convert_to_tensor(X_u, dtype="float32")
-    self.u = tf.convert_to_tensor(u, dtype="float32")
+    self.X_u = tf.convert_to_tensor(X_u, dtype=self.dtype)
+    self.u = tf.convert_to_tensor(u, dtype=self.dtype)
     # Separating the collocation coordinates (here the same as data)
-    self.x_f = tf.convert_to_tensor(X_u[:, 0:1], dtype="float32")
-    self.t_f = tf.convert_to_tensor(X_u[:, 1:2], dtype="float32")
+    self.x_f = tf.convert_to_tensor(X_u[:, 0:1], dtype=self.dtype)
+    self.t_f = tf.convert_to_tensor(X_u[:, 1:2], dtype=self.dtype)
 
     # Training loop
     for epoch in range(epochs):
@@ -134,7 +125,7 @@ class PhysicsInformedNN(object):
     f_star = self.__f_model()
     return u_star, f_star
 
-#%% RUNNING THE MODEL and PLOTTING
+#%% TRAINING THE MODEL
 
 # Getting the data
 path = os.path.join(appDataPath, "burgers_shock.mat")
@@ -150,72 +141,7 @@ pinn.fit(X_u_train, u_train, epochs)
 # Getting the model predictions, from the same (x,t) that the predictions were previously gotten from
 u_pred, f_pred = pinn.predict(X_star)
 
-# Interpolating the results on the whole (x,t) domain.
-# griddata(points, values, points at which to interpolate, method)
-U_pred = griddata(X_star, u_pred.numpy().flatten(), (X, T), method='cubic')
+#%% PLOTTING
 
-# Creating the figures
-fig, ax = newfig(1.0, 1.1)
-ax.axis('off')
-
-####### Row 0: u(t,x) ##################    
-gs0 = gridspec.GridSpec(1, 2)
-gs0.update(top=1-0.06, bottom=1-1/3, left=0.15, right=0.85, wspace=0)
-ax = plt.subplot(gs0[:, :])
-
-h = ax.imshow(U_pred.T, interpolation='nearest', cmap='rainbow', 
-              extent=[t.min(), t.max(), x.min(), x.max()], 
-              origin='lower', aspect='auto')
-divider = make_axes_locatable(ax)
-cax = divider.append_axes("right", size="5%", pad=0.05)
-fig.colorbar(h, cax=cax)
-
-ax.plot(X_u_train[:,1], X_u_train[:,0], 'kx', label = 'Data (%d points)' % (u_train.shape[0]), markersize = 4, clip_on = False)
-
-line = np.linspace(x.min(), x.max(), 2)[:,None]
-ax.plot(t[25]*np.ones((2,1)), line, 'w-', linewidth = 1)
-ax.plot(t[50]*np.ones((2,1)), line, 'w-', linewidth = 1)
-ax.plot(t[75]*np.ones((2,1)), line, 'w-', linewidth = 1)    
-
-ax.set_xlabel('$t$')
-ax.set_ylabel('$x$')
-ax.legend(frameon=False, loc = 'best')
-ax.set_title('$u(t,x)$', fontsize = 10)
-
-####### Row 1: u(t,x) slices ##################    
-gs1 = gridspec.GridSpec(1, 3)
-gs1.update(top=1-1/3, bottom=0, left=0.1, right=0.9, wspace=0.5)
-
-ax = plt.subplot(gs1[0, 0])
-ax.plot(x,Exact_u[25,:], 'b-', linewidth = 2, label = 'Exact')       
-ax.plot(x,U_pred[25,:], 'r--', linewidth = 2, label = 'Prediction')
-ax.set_xlabel('$x$')
-ax.set_ylabel('$u(t,x)$')    
-ax.set_title('$t = 0.25$', fontsize = 10)
-ax.axis('square')
-ax.set_xlim([-1.1,1.1])
-ax.set_ylim([-1.1,1.1])
-
-ax = plt.subplot(gs1[0, 1])
-ax.plot(x,Exact_u[50,:], 'b-', linewidth = 2, label = 'Exact')       
-ax.plot(x,U_pred[50,:], 'r--', linewidth = 2, label = 'Prediction')
-ax.set_xlabel('$x$')
-ax.set_ylabel('$u(t,x)$')
-ax.axis('square')
-ax.set_xlim([-1.1,1.1])
-ax.set_ylim([-1.1,1.1])
-ax.set_title('$t = 0.50$', fontsize = 10)
-ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.35), ncol=5, frameon=False)
-
-ax = plt.subplot(gs1[0, 2])
-ax.plot(x,Exact_u[75,:], 'b-', linewidth = 2, label = 'Exact')       
-ax.plot(x,U_pred[75,:], 'r--', linewidth = 2, label = 'Prediction')
-ax.set_xlabel('$x$')
-ax.set_ylabel('$u(t,x)$')
-ax.axis('square')
-ax.set_xlim([-1.1,1.1])
-ax.set_ylim([-1.1,1.1])    
-ax.set_title('$t = 0.75$', fontsize = 10)
-
-plt.show()
-# savefig('./inf_cont_burgers')
+plot_results(X_star, u_pred.numpy().flatten(), X_u_train, u_train,
+  Exact_u, X, T, x, t)
