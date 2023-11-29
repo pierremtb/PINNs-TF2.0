dc6ed5b448d0c3a693ae1e2c708ffdab10302afa
adityaencoder18 <adityaencoder18>
2023-11-29 06:28:23 +0000

    dum

---
+++ 1dcomplex-schrodinger/ipy_nbs/dummy_schrod.py
@@ -0,0 +1,152 @@
+# Databricks notebook source
+# dummy
+print('inside dummy_schrod notebook')
+
+# COMMAND ----------
+
+
+import scipy.io
+import numpy as np
+import tensorflow as tf
+import time
+from datetime import datetime
+from pyDOE import lhs
+import os
+import json
+import sys
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+from scipy.interpolate import griddata
+import matplotlib.gridspec as gridspec
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+sys.path.append("utils")
+from plotting import newfig, savefig, saveResultDir
+
+def prep_data(path, N_0, N_b, N_f, noise):
+        
+    data = scipy.io.loadmat(path)
+    
+    t = data['tt'].flatten()[:,None]
+    x = data['x'].flatten()[:,None]
+    Exact = data['uu']
+    Exact_u = np.real(Exact)
+    Exact_v = np.imag(Exact)
+    Exact_h = np.sqrt(Exact_u**2 + Exact_v**2)
+    
+    X, T = np.meshgrid(x,t)
+    
+    X_star = np.hstack((X.flatten()[:,None], T.flatten()[:,None]))
+    u_star = Exact_u.T.flatten()[:,None]
+    v_star = Exact_v.T.flatten()[:,None]
+    h_star = Exact_h.T.flatten()[:,None]
+
+    # lb = X_star.min(axis=0)
+    # ub = X_star.max(axis=0) 
+    lb = np.array([-5.0, 0.0])
+    ub = np.array([5.0, np.pi/2])
+    
+    ###########################
+    
+    idx_x = np.random.choice(x.shape[0], N_0, replace=False)
+    x0 = x[idx_x,:]
+    u0 = Exact_u[idx_x,0:1]
+    v0 = Exact_v[idx_x,0:1]
+    
+    idx_t = np.random.choice(t.shape[0], N_b, replace=False)
+    tb = t[idx_t,:]
+
+    # X0 = np.hstack((x0, tb))
+    X0 = np.concatenate((x0, 0*x0), 1) # (x0, 0)
+    H0 = np.hstack((u0, v0))
+    
+    X_f = lb + (ub-lb)*lhs(2, N_f)
+
+    return x, t, X, T, Exact_u, Exact_v, Exact_h, \
+        X_star, u_star, v_star, h_star, X_f, ub, lb, tb, x0, u0, v0, X0, H0
+
+
+def plot_inf_cont_results(X_star, u_pred, v_pred, h_pred, Exact_h, X, T, x, t, ub, lb, x0, tb, save_path=None, save_hp=None):
+
+    # Interpolating the results on the whole (x,t) domain.
+    # griddata(points, values, points at which to interpolate, method)
+    U_pred = griddata(X_star, u_pred.flatten(), (X, T), method='cubic')
+    V_pred = griddata(X_star, v_pred.flatten(), (X, T), method='cubic')
+    H_pred = griddata(X_star, h_pred.flatten(), (X, T), method='cubic')
+
+    # FU_pred = griddata(X_star, f_u_pred.flatten(), (X, T), method='cubic')
+    # FV_pred = griddata(X_star, f_v_pred.flatten(), (X, T), method='cubic')     
+
+    X0 = np.concatenate((x0, 0*x0), 1) # (x0, 0)
+    X_lb = np.concatenate((0*tb + lb[0], tb), 1) # (lb[0], tb)
+    X_ub = np.concatenate((0*tb + ub[0], tb), 1) # (ub[0], tb)
+    X_u_train = np.vstack([X0, X_lb, X_ub])
+
+    fig, ax = newfig(1.0, 0.9)
+    ax.axis('off')
+    
+    ####### Row 0: h(t,x) ##################    
+    gs0 = gridspec.GridSpec(1, 2)
+    gs0.update(top=1-0.06, bottom=1-1/3, left=0.15, right=0.85, wspace=0)
+    ax = plt.subplot(gs0[:, :])
+    
+    h = ax.imshow(H_pred.T, interpolation='nearest', cmap='YlGnBu', 
+                  extent=[lb[1], ub[1], lb[0], ub[0]], 
+                  origin='lower', aspect='auto')
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    fig.colorbar(h, cax=cax)
+    
+    ax.plot(X_u_train[:,1], X_u_train[:,0], 'kx', label = 'Data (%d points)' % (X_u_train.shape[0]), markersize = 4, clip_on = False)
+    
+    line = np.linspace(x.min(), x.max(), 2)[:,None]
+    ax.plot(t[75]*np.ones((2,1)), line, 'k--', linewidth = 1)
+    ax.plot(t[100]*np.ones((2,1)), line, 'k--', linewidth = 1)
+    ax.plot(t[125]*np.ones((2,1)), line, 'k--', linewidth = 1)    
+    
+    ax.set_xlabel('$t$')
+    ax.set_ylabel('$x$')
+    leg = ax.legend(frameon=False, loc = 'best')
+#    plt.setp(leg.get_texts(), color='w')
+    ax.set_title('$|h(t,x)|$', fontsize = 10)
+    
+    ####### Row 1: h(t,x) slices ##################    
+    gs1 = gridspec.GridSpec(1, 3)
+    gs1.update(top=1-1/3, bottom=0, left=0.1, right=0.9, wspace=0.5)
+    
+    ax = plt.subplot(gs1[0, 0])
+    ax.plot(x,Exact_h[:,75], 'b-', linewidth = 2, label = 'Exact')       
+    ax.plot(x,H_pred[75,:], 'r--', linewidth = 2, label = 'Prediction')
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$|h(t,x)|$')    
+    ax.set_title('$t = %.2f$' % (t[75]), fontsize = 10)
+    ax.axis('square')
+    ax.set_xlim([-5.1,5.1])
+    ax.set_ylim([-0.1,5.1])
+    
+    ax = plt.subplot(gs1[0, 1])
+    ax.plot(x,Exact_h[:,100], 'b-', linewidth = 2, label = 'Exact')       
+    ax.plot(x,H_pred[100,:], 'r--', linewidth = 2, label = 'Prediction')
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$|h(t,x)|$')
+    ax.axis('square')
+    ax.set_xlim([-5.1,5.1])
+    ax.set_ylim([-0.1,5.1])
+    ax.set_title('$t = %.2f$' % (t[100]), fontsize = 10)
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.8), ncol=5, frameon=False)
+    
+    ax = plt.subplot(gs1[0, 2])
+    ax.plot(x,Exact_h[:,125], 'b-', linewidth = 2, label = 'Exact')       
+    ax.plot(x,H_pred[125,:], 'r--', linewidth = 2, label = 'Prediction')
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$|h(t,x)|$')
+    ax.axis('square')
+    ax.set_xlim([-5.1,5.1])
+    ax.set_ylim([-0.1,5.1])    
+    ax.set_title('$t = %.2f$' % (t[125]), fontsize = 10)
+
+    if save_path != None and save_hp != None:
+      saveResultDir(save_path, save_hp)
+
+    else:
+      plt.show()
